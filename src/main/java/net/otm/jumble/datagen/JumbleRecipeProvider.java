a91c43b7fc782a674967bce5a344c8c0557e65df
OTM2012 <owentmorse@icloud.com>
2024-01-13 01:59:42 -0500

    added smithing recipes for celestium tools

--- src/main/java/net/otm/jumble/datagen/JumbleRecipeProvider.java
+++ src/main/java/net/otm/jumble/datagen/JumbleRecipeProvider.java
@@ -3,8 +3,12 @@ package net.otm.jumble.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
-import net.minecraft.item.ItemConvertible;
+import net.minecraft.data.server.recipe.SmithingTransformRecipeJsonBuilder;
+import net.minecraft.item.*;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
+import net.otm.jumble.Jumble;
 import net.otm.jumble.block.JumbleBlocks;
 import net.otm.jumble.item.JumbleItems;
 
@@ -14,6 +18,7 @@ import java.util.function.Consumer;
 public class JumbleRecipeProvider extends FabricRecipeProvider {
     private static final List<ItemConvertible> CELESTIUM_SMELTABLE = List.of(
         JumbleBlocks.CELESTIUM_BLOCK, JumbleBlocks.CELESTIUM_ORE);
+
     public JumbleRecipeProvider(FabricDataOutput output) {
         super(output);
     }
@@ -25,6 +30,20 @@ public class JumbleRecipeProvider extends FabricRecipeProvider {
 
         offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, JumbleItems.CELESTIUM_INGOT, RecipeCategory.DECORATIONS, JumbleBlocks.CELESTIUM_BLOCK);
 
+        SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(JumbleItems.CELESTIUM_UPGRADE_SMITHING_TEMPLATE), Ingredient.ofItems(Items.DIAMOND_PICKAXE),Ingredient.ofItems(JumbleItems.CELESTIUM_INGOT), RecipeCategory.MISC, JumbleItems.CELESTIUM_PICKAXE)
+                .criterion(hasItem(JumbleItems.CELESTIUM_INGOT), conditionsFromItem(JumbleItems.CELESTIUM_INGOT)).offerTo(exporter, new Identifier(Jumble.MOD_ID, "celestium_pickaxe_smithing"));
+
+        SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(JumbleItems.CELESTIUM_UPGRADE_SMITHING_TEMPLATE), Ingredient.ofItems(Items.DIAMOND_AXE),Ingredient.ofItems(JumbleItems.CELESTIUM_INGOT), RecipeCategory.MISC, JumbleItems.CELESTIUM_AXE)
+                .criterion(hasItem(JumbleItems.CELESTIUM_INGOT), conditionsFromItem(JumbleItems.CELESTIUM_INGOT)).offerTo(exporter, new Identifier(Jumble.MOD_ID, "celestium_axe_smithing"));
+
+        SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(JumbleItems.CELESTIUM_UPGRADE_SMITHING_TEMPLATE), Ingredient.ofItems(Items.DIAMOND_SHOVEL),Ingredient.ofItems(JumbleItems.CELESTIUM_INGOT), RecipeCategory.MISC, JumbleItems.CELESTIUM_SHOVEL)
+                .criterion(hasItem(JumbleItems.CELESTIUM_INGOT), conditionsFromItem(JumbleItems.CELESTIUM_INGOT)).offerTo(exporter, new Identifier(Jumble.MOD_ID, "celestium_shovel_smithing"));
+
+        SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(JumbleItems.CELESTIUM_UPGRADE_SMITHING_TEMPLATE), Ingredient.ofItems(Items.DIAMOND_SWORD),Ingredient.ofItems(JumbleItems.CELESTIUM_INGOT), RecipeCategory.MISC, JumbleItems.CELESTIUM_SWORD)
+                .criterion(hasItem(JumbleItems.CELESTIUM_INGOT), conditionsFromItem(JumbleItems.CELESTIUM_INGOT)).offerTo(exporter, new Identifier(Jumble.MOD_ID, "celestium_sword_smithing"));
+
+        SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(JumbleItems.CELESTIUM_UPGRADE_SMITHING_TEMPLATE), Ingredient.ofItems(Items.DIAMOND_HOE),Ingredient.ofItems(JumbleItems.CELESTIUM_INGOT), RecipeCategory.MISC, JumbleItems.CELESTIUM_HOE)
+                .criterion(hasItem(JumbleItems.CELESTIUM_INGOT), conditionsFromItem(JumbleItems.CELESTIUM_INGOT)).offerTo(exporter, new Identifier(Jumble.MOD_ID, "celestium_hoe_smithing"));
 
     }
 }
